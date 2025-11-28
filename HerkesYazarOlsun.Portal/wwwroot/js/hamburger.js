fb0559b6b8e51de83d8123cfe718d4e186d479e5
Mansur Emin Kaya <kayamansur61@gmail.com>
2025-11-28 16:33:09 +0300

    hamburger menu js revize edildi.

---
+++ HerkesYazarOlsun.Portal/wwwroot/js/hamburger.js
@@ -0,0 +1,46 @@
+﻿ $(document).ready(function () {
+        // Desktop: hover ile aç/kapa
+        $('.dropdown').hover(
+            function () {
+                if ($(window).width() >= 768) {
+                    $(this).addClass('open');
+                }
+            },
+            function () {
+                if ($(window).width() >= 768) {
+                    $(this).removeClass('open');
+                }
+            }
+        );
+
+        // Mobil: dropdown toggle
+        $('.dropdown-toggle').click(function (e) {
+            if ($(window).width() < 768) {
+                e.preventDefault();
+                var $parent = $(this).parent('.dropdown');
+                $parent.toggleClass('open');
+            }
+        });
+
+        // Hamburger toggle
+        $('.navbar-toggle').click(function () {
+            $('#main-navbar').slideToggle();
+        });
+
+        // Sayfa resize
+        $(window).resize(function () {
+            if ($(window).width() >= 768) {
+                $('#main-navbar').show().css('max-height', '');
+            } else {
+                $('#main-navbar').hide().css('max-height', '80vh');
+                $('.dropdown').removeClass('open');
+            }
+        });
+
+        // Başlangıç kontrolü
+        if ($(window).width() >= 768) {
+            $('#main-navbar').show();
+        } else {
+            $('#main-navbar').hide().css('max-height', '80vh');
+        }
+    });
